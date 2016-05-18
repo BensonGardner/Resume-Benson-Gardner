29e202358a2860837579c8e1134ac5f3959f7b53
Benson Gardner <bensongardner@gmail.com>
2016-05-18 16:17:56 -0500

    Fix filterDuties function so that it actually filters.

--- js/helper.js
+++ js/helper.js
@@ -14,7 +14,7 @@ var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
 var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item" data-skill="%data%"><span class="white-text"><a href="#">%data%</a></span></li>';
+var HTMLskills = '<li class="flex-item" data-skill="%data%"><span class="white-text"><a href="javascript:;">%data%</a></span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data% - ';
